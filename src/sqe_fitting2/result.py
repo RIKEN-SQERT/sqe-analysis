a61bbb0d64762195818a7aad5efd34e775d584b6
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-08-12 20:18:03 +0900

    More sketching of result API

--- src/sqe_fitting2/result.py
+++ src/sqe_fitting2/result.py
@@ -1,21 +1,34 @@
 import xarray as xr
-from dataclasses import dataclass
+from xarray.core.datatree import DatasetView
 
 
-#@dataclass(frozen=True) # TODO: is dataclass appropriate
+# @dataclass(frozen=True) # TODO: is dataclass appropriate
 class AnalysisResult:
     """
     A thin wrapper around an Xarray DataTree.
+
+    This wrapper exists so that we can do type-checking on the datasets stored
+    in the DataTree and verify that they follow the result schema.
+
+    Args:
+        params: The qantities of interest of the analysis. The Dataset should
+            have one data variable for each parameter.
+        params_std: Estimates of the standard deviations of the extracted
+            parameters, if available.
+        intermediate_results: Additional analysis results that are needed for
+            visualization, but not direct quantities of interest of the analysis
+        debug_results: Additional analysis results that are useful for troubleshooting the analysis, but
     """
+
     def __init__(
         self,
         params: xr.Dataset,
         params_std: xr.Dataset | None = None,
-        intermediate_results: xr.Dataset | xr.DataTree | None = None,
-        debug_results: xr.Dataset | xr.DataTree | None = None,
+        intermediate_results: xr.Dataset | None = None,
+        debug_results: xr.Dataset | None = None,
     ):
-        self._data = xr.DataTree(
-            children=dict(
+        self._data: xr.DataTree = xr.DataTree(
+            children=dict(  # noqa: C408
                 params=xr.DataTree(params),
                 params_std=xr.DataTree(params_std),
                 intermediate_results=xr.DataTree(intermediate_results),
@@ -24,28 +37,30 @@ class AnalysisResult:
         )
 
     @property
-    def params(self) -> xr.Dataset:
+    def params(self) -> DatasetView:
         # TODO: docstring...
         return self._data.params.dataset
 
     @property
-    def params_std(self) -> xr.Dataset | None:
+    def params_std(self) -> DatasetView | None:  # TODO: check None return type...
         return self._data.params_std.dataset
-    
-    @property
-    def intermediate_results(self) -> xr.DatasetView:
-        return self._data.intermediate_results
 
     @property
-    def debug_results(self) -> xr.DatasetView:
-        return self._data.intermediate_results
+    def intermediate_results(self) -> DatasetView | None:
+        return self._data.intermediate_results.dataset
 
-    def to_netcdf(self):
+    @property
+    def debug_results(self) -> DatasetView | None:
+        return self._data.debug_results.dataset
+
+    def to_netcdf(self, path: ...):
         raise NotImplementedError
 
     def from_netcdf(self, path: ...):
-        # TODO: function to validate the schema
+        # TODO: option / function to validate the schema
         raise NotImplementedError
-    
+
     def _repr_html_(self):
-        return self._data._repr_html_()
+        return self._data._repr_html_().replace(
+            "xarray.DataTree", f"{self.__class__.__name__} (xarray.DataTree)"
+        )
