f6d7ca5a70b8cf98f706bee57650d41e06884513
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-08-12 13:50:48 +0900

    Fix type hint of dim in project_complex
    Converting to list when passing the dims to mean() like it was before
    doesn't work if we pass multiple dimensions to `project_complex`. Needed
    to rework return type of `sorted_dims` and `longest_dim` to fix type
    checking but assuming `str` is decent for now.

--- src/sqe_fitting2/xr_util.py
+++ src/sqe_fitting2/xr_util.py
@@ -2,21 +2,25 @@
 Small utility functions for dealing with xarray arrays
 """
 
-from collections.abc import Hashable
+from typing import cast
 
 import xarray as xr
 
 
-def sorted_dims(data: xr.DataArray | xr.Dataset) -> list[Hashable]:
+def sorted_dims(data: xr.DataArray | xr.Dataset) -> list[str]:
     """
     Return the names of the dimensions of a Dataset or DataArray, with the
     shortest dimension first.
     """
     kvs = sorted(data.sizes.items(), key=lambda kv: kv[1])
-    return [kv[0] for kv in kvs]
+    # Cast dimensions to str to make type checking simpler. In principle, the
+    # dimensions can also be tuples, but this is very rare. We can figure out
+    # the correct type hints if we actually encounter a situation where we need
+    # tuples.
+    return [cast(str, kv[0]) for kv in kvs]
 
 
-def longest_dim(data: xr.DataArray | xr.Dataset) -> Hashable:
+def longest_dim(data: xr.DataArray | xr.Dataset) -> str:
     """
     Return the longest dimension of the Dataset or DataArray. If the data is
     zero-dimensional, raises a ValueError.
