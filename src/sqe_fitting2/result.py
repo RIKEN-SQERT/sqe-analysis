f5ca768bd853fff65f430ae90ca2a847ecc94ba1
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-08-14 16:43:38 +0900

    Change from DataTree wrapper to dataclass of datasets
    Docstring formatting is still a bit of a question (Attributes in
    Google-style class docstring vs inline docstrings of attributes; both
    have downsides). Also IPython `?` help syntax doesn't work in either
    case. But will think about that later a bit more. Also _repr_html_ needs
    to be (re)implemented

--- src/sqe_fitting2/result.py
+++ src/sqe_fitting2/result.py
@@ -1,16 +1,17 @@
+from dataclasses import dataclass
+
 import xarray as xr
-from xarray.core.datatree import DatasetView
 
 
+@dataclass(kw_only=True)
 class AnalysisResult:
     """
-    A thin wrapper around an Xarray DataTree.
+    Data class for storing analysis results.
 
-    This wrapper exists so that we can do type-checking and autocomplete on the
-    datasets stored in the DataTree, and verify that they follow the result
-    schema.
+    All attributes are Xarray Datasets so that their values can be
+    multidimensional arrays.
 
-    Args:
+    Attributes:
         params: The qantities of interest of the analysis. The Dataset should
             have one data variable for each parameter.
         params_std: Estimates of the standard deviations of the extracted
@@ -22,42 +23,10 @@ class AnalysisResult:
             These may not be saved when ??saving the result as netcdf??.
     """
 
-    def __init__(
-        self,
-        params: xr.Dataset,
-        *,
-        params_std: xr.Dataset | None = None,
-        intermediate_results: xr.Dataset | None = None,
-        debug_results: xr.Dataset | None = None,
-    ):
-        children: dict[str, xr.DataTree] = {"params": xr.DataTree(params)}
-        if params_std is not None:
-            children["params_std"] = xr.DataTree(params_std)
-        if intermediate_results is not None:
-            children["intermediate_results"] = xr.DataTree(intermediate_results)
-        if debug_results is not None:
-            children["debug_results"] = xr.DataTree(debug_results)
-        self._data: xr.DataTree = xr.DataTree(children=children)
-
-    @property
-    def params(self) -> DatasetView:
-        # TODO: docstring...
-        return self._data.params.dataset
-
-    @property
-    def params_std(self) -> DatasetView | None:
-        node = self._data.get("params_std")
-        return node.dataset if node is not None else None
-
-    @property
-    def intermediate_results(self) -> DatasetView | None:
-        node = self._data.get("intermediate_results")
-        return node.dataset if node is not None else None
-
-    @property
-    def debug_results(self) -> DatasetView | None:
-        node = self._data.get("debug_results")
-        return node.dataset if node is not None else None
+    params: xr.Dataset
+    params_std: xr.Dataset | None = None
+    intermediate_results: xr.Dataset | None = None
+    debug_results: xr.Dataset | None = None
 
     def to_netcdf(self, path: ...):
         raise NotImplementedError
@@ -67,26 +36,23 @@ class AnalysisResult:
         # TODO: option / function to validate the schema
         raise NotImplementedError
 
-    def _repr_html_(self):
-        return self._data._repr_html_().replace(
-            "xarray.DataTree", f"{self.__class__.__name__} (xarray.DataTree)"
-        )
+    # TODO: reimplement this after changing from DataTree wrapper to dataclass
+    # def _repr_html_(self):
+    #    return self._data._repr_html_().replace(
+    #        "xarray.DataTree", f"{self.__class__.__name__} (xarray.DataTree)"
+    #    )
 
 
+@dataclass(kw_only=True)
 class CurvefitAnalysisResult(AnalysisResult):
     """
     Specialized analysis result for curve fitting-based analysis.
 
-    Args:
-        params: The quantities of interest of the analysis. These may be
-            parameters of the model function, but may also contain additional
-            derived quantities, and may not necessarily contain all model
-            function parameters if they are not of interest. To evaluate the
-            model function, use `fit_params` instead.
-        fit_params: The parameters of the model function that minimize the error
-            between the model prediction and the data. These should always
-            exactly match the arguments of the model function, so that they can
-            be used to evaluate it.
+    Attributes:
+        fit_params: Dataset containing the parameters of the model function that
+            minimize the error between the model prediction and the data. These
+            should always exactly match the arguments of the model function, so
+            that they can be used to evaluate it.
         fit_params_guess: The initial guess used for the fitting, either
             calculated using the `guess` method or explicitly passed as
             arguments. May not contain all parameters of the model function, so
@@ -94,43 +60,12 @@ class CurvefitAnalysisResult(AnalysisResult):
             values.
     """
 
-    def __init__(
-        self,
-        params: xr.Dataset,
-        fit_params: xr.Dataset,
-        *,
-        params_std: xr.Dataset | None = None,
-        # TODO: fit_params_std for the uncertainties of the fit parameters
-        intermediate_results: xr.Dataset | None = None,
-        fit_params_guess: xr.Dataset | None = None,
-        debug_results: xr.Dataset | None = None,
-    ):
-        super().__init__(
-            params=params,
-            params_std=params_std,
-            intermediate_results=intermediate_results,
-            debug_results=debug_results,
-        )
-        # TODO: validate that the parameters that are present both in params and
-        # fit_params are equal
-        data = self._data.assign(fit_params=xr.DataTree(fit_params))
-        if fit_params_guess is not None:
-            data = data.assign(fit_params_guess=xr.DataTree(fit_params_guess))
-        self._data = data
+    # TODO: validate that the parameters that are present both in params and
+    # fit_params are equal
+    # TODO: example that shows how to evaluate the model function with `fit_params`
 
-    @property
-    def fit_params(self) -> DatasetView:
-        """
-        Dataset containing the parameters of the model function that minimize
-        the error between the model prediction and the data. This can be used to
-        evaluate the model function:
+    fit_params: xr.Dataset
 
-            >>> result = MyCurvefitAnalysis.run(data, dim="x")
-            >>> fit_eval = MyCurvefitAnalysis.func(data.x, **result.fit_params)
-        """
-        return self._data.fit_params.dataset
+    # TODO: fit_params_std for the uncerainties of the fit parameters
 
-    @property
-    def fit_params_guess(self) -> DatasetView | None:
-        node = self._data.get("fit_params_guess")
-        return node.dataset if node is not None else None
+    fit_params_guess: xr.Dataset | None = None
