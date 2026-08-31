86f9b0696778c5375a8dde3889f74e1358b8f720
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-08-31 18:17:57 +0900

    Fix infinite loop with myst-nb + sphinx-autobuild

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,14 @@ extensions = [
     "myst_nb",  # to enable {code-cell} - also activates myst_parser
 ]
 
+# myst-nb creates new ipynb files under _build, which sphinx treats as new files
+# so sphinx-autobuild goes into an infinite loop if we don't have this.
+# see also https://myst-nb.readthedocs.io/en/latest/computation/execute.html,
+# although the default seems fine for now.
+exclude_patterns = [
+    "_build"
+]
+
 suppress_warnings = [
     "mystnb.unknown_mime_type"  # "WARNING: skipping unknown output mime type: application/vnd.holoviews_load.v0+json"
 ]
