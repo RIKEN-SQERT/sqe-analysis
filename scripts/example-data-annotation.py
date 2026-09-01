0dae670d9d8dddc8a1acf6b9d414fdda3ca3a014
Márton Gunyhó <andras.gunyho@riken.jp>
2026-09-01 17:05:26 +0900

    Add script for cleaning up & annotating example data

---
+++ scripts/example-data-annotation.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.19.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## Example data cleanup & annotation
+#
+# This notebook shows an example of importing example data from an external experiment framework and annotating it with the appropriate metadata. It can be used as a template for performing this kind of cleanup.
+
+
