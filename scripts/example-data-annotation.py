331fddefe8f04e831f4d6d8fbdd37c870022e0d3
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-09-02 11:35:33 +0900

    Actually annotate some data in example notebook & add to example datasets

--- scripts/example-data-annotation.py
+++ scripts/example-data-annotation.py
@@ -17,4 +17,62 @@
 #
 # This notebook shows an example of importing example data from an external experiment framework and annotating it with the appropriate metadata. It can be used as a template for performing this kind of cleanup.
 
+# +
+from pathlib import Path
 
+import hvplot.xarray
+import xarray as xr
+
+from sqe_analysis.example_data import open_dataset as open_example_dataset
+from sqe_analysis.example_data import validate_metadata
+
+LICENSE = "CC BY-SA 4.0"
+# -
+
+# data has been exported from the measurement framework to exported_data/ folder
+ds = xr.open_dataset("exported_data/1373-CheckKappaChi_Q72.nc")
+ds
+
+# check what the data looks like, so we can refer to it while writing the description
+ds[next(iter(ds.data_vars))].real.hvplot(
+    x="resonator_drive_frequency_shift",
+    y="qubit_drive_frequency_shift",
+).layout().cols(1)
+
+# +
+ds_annotated = (
+    ds
+    .drop_attrs(deep=False) # remove top-level attributes but keep e.g. units in coordinate axes
+    .assign_attrs(
+        title="AC stark shift vs resonator frequency and qubit state",
+        description=(
+            "Resonator transmission after populating the resonator and applying a qubit probe pulse, as a function of the resonator population pulse frequency and qubit probe pulse frequency, with the qubit initially prepared in g & e. "
+            "Complex-valued three-dimensional data. "
+            "Also known as chi-kappa-power measurement (see Sank et al. Phys. Rev. Applied 23, 024055 (2025)). "
+            "Measured with QuEL-SE1. "
+        ).strip(),
+        quality_notes=(
+            "Good SNR. There is some distortion around resonator drive frequency -30 MHz and +10 MHz with qubit in the e-state, and some bands along the qubit frequency shift near -10 to 0 MHz resonator frequency. "
+        ).strip(),
+
+        # based on the original metadata in the netcdf file
+        source="RIKEN, SQERT XLD3 CD25 FY2023 144Q 3rd lot No6 dataset 1373",
+        #author="Adrian Hesse / RIKEN",
+        license=LICENSE,
+        
+        source_type="experimental",
+    )
+)
+
+validate_metadata(ds_annotated)
+
+ds_annotated
+# -
+
+# check metadata of existing example data for comparison
+open_example_dataset("t1_flip-distorted-RX4_59")
+
+# save the data in the examples folder of the library
+path = Path("../src/sqe_analysis/example_data/ac_stark_shift_vs_resonator_frequency-good_snr_glitch-RX3_1373.nc")
+assert not path.exists()
+ds_annotated.to_netcdf(path, engine="h5netcdf")
